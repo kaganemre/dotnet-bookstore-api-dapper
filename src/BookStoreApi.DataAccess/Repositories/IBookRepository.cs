15f6a8e4e6e714c7be2ed32d0663ae44e562744b
Emre Kağan Erde <emrekaganerde@outlook.com>
2026-07-13 10:59:50 +0300

    refactor: remove optional CancellationToken defaults from IBookRepository

--- src/BookStoreApi.DataAccess/Repositories/IBookRepository.cs
+++ src/BookStoreApi.DataAccess/Repositories/IBookRepository.cs
@@ -4,9 +4,9 @@ namespace BookStoreApi.DataAccess.Repositories;
 
 public interface IBookRepository
 {
-    Task<IEnumerable<Book>> GetAllAsync(CancellationToken ct = default);
-    Task<Book?> GetByIdAsync(Guid id, CancellationToken ct = default);
-    Task<Guid> CreateAsync(Book book, CancellationToken ct = default);
-    Task<bool> UpdateAsync(Book book, CancellationToken ct = default);
-    Task<bool> DeleteAsync(Guid id, CancellationToken ct = default);
+    Task<IEnumerable<Book>> GetAllAsync(CancellationToken ct);
+    Task<Book?> GetByIdAsync(Guid id, CancellationToken ct);
+    Task<Guid> CreateAsync(Book book, CancellationToken ct);
+    Task<bool> UpdateAsync(Book book, CancellationToken ct);
+    Task<bool> DeleteAsync(Guid id, CancellationToken ct);
 }
